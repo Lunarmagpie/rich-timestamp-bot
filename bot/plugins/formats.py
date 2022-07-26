eb5d7cdfd8aaf34017b439b2c8d4844650f826bb
Lunarmagpie <Bambolambo0@gmail.com>
2022-07-26 05:54:41 -0400

    optimize embeds

--- bot/plugins/formats.py
+++ bot/plugins/formats.py
@@ -1,22 +1,17 @@
 import crescent
-import hikari
 from crescent.ext import docstrings
 
 from bot.util import TIME_FORMATS
 
 plugin = crescent.Plugin()
 
-resp_embed = hikari.Embed()
-
-
-format_examples = "\n".join(
+FORMAT_EXAMPLES = "\n".join(
     f"`{name.ljust(15, ' ')}`\t<t:1658851200:{letter}>" for name, letter in TIME_FORMATS
 )
-resp_embed.add_field("Formats", value=format_examples)
 
 
 @plugin.include
 @docstrings.parse_doc
 @crescent.command
 async def formats(ctx: crescent.Context) -> None:
-    await ctx.respond(format_examples)
+    await ctx.respond(FORMAT_EXAMPLES)
--- bot/plugins/info.py
+++ bot/plugins/info.py
@@ -7,6 +7,17 @@ from bot.config import CONFIG
 plugin = crescent.Plugin()
 
 
+EMBED = (
+    hikari.Embed(title="The Time")
+    .add_field("Source code", "https://github.com/Lunarmagpie/rich-timestamp-bot")
+    .add_field("Author", "<@318076068290494466>")
+    .add_field(
+        "Invite this bot to your server",
+        f"[Click here]({CONFIG.invite_link})",
+    )
+)
+
+
 @plugin.include
 @docstrings.parse_doc
 @crescent.command
@@ -14,14 +25,5 @@ async def info(ctx: crescent.Context) -> None:
     """
     View the info for this discord bot.
     """
-    embed = (
-        hikari.Embed(title="The Time")
-        .add_field("Source code", "https://github.com/Lunarmagpie/rich-timestamp-bot")
-        .add_field("Author", "<@318076068290494466>")
-        .add_field(
-            "Invite this bot to your server",
-            f"[Click here]({CONFIG.invite_link})",
-        )
-    )
 
-    await ctx.respond(embed=embed)
+    await ctx.respond(embed=EMBED)
